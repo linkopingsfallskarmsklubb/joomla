6d6ead54c5ab4d70111ece7bd38b970a086f5b5c
Christian Svensson <blue@cmd.nu>
2015-02-26 16:55:06 +0000

    Backup script

---
+++ scripts/backup.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -e
+
+mysqldump -u backup --all-databases > /tmp/mysqldump.sql 2> /dev/null
+
+OUT=/tmp/$(date +%Y-%m-%d_%H%M)-lfk.tar.xz
+tar -Jcf $OUT /opt/bitnami/apps/joomla/htdocs/ /tmp/mysqldump.sql 2> /dev/null
+
+rm -f /tmp/mysqldump.sql
+
+/usr/local/bin/gsutil cp $OUT gs://lfk/backup/ 2> /dev/null
