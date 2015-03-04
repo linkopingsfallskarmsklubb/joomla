308eeeee91203c90acae20486e58b21a7ed4e006
Christian Svensson <blue@cmd.nu>
2015-03-04 23:49:56 +0000

    Remove stuff we don't need

--- medlemslogin/logga_in_head.php
+++
@@ -1,9 +0,0 @@
-  <!-- Main style sheet -->
-  <link rel="stylesheet" type="text/css" href="<?=DIR_REL?>/single_pages/includes/styles.css" />
-  <link rel="stylesheet" type="text/css" href="<?=DIR_REL?>/single_pages/medlemslogin/logga_in.css" />
-
-  <!-- Javascript functions -->
-  <script type="text/javascript" src="<?=DIR_REL?>/single_pages/includes/scripts.js"></script>
-  <script type="text/javascript" src="<?=DIR_REL?>/single_pages/medlemslogin/logga_in_scripts.js"></script>
-
-
--- medlemslogin/logga_ut_head.php
+++
@@ -1,4 +0,0 @@
-  <!-- Javascript functions -->
-  <script type="text/javascript" src="<?=DIR_REL?>/single_pages/includes/scripts.js"></script>
-  <script type="text/javascript" src="<?=DIR_REL?>/single_pages/medlemslogin/logga_ut_scripts.js"></script>
-
